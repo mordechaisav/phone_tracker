7703476d0769d563fe71d32b77cb2ea527c41afb
mordechaisav <mordechy7@gmail.com>
2024-11-28 16:54:32 +0200

    Added config file for connected to neo4j

--- database/config.py
+++ database/config.py
@@ -1,9 +1,6 @@
 from neo4j import GraphDatabase
-import redis
 
-
-
-uri = "bolt://neo4j:7687"
+uri = "bolt://localhost:7687"
 username = "neo4j"
-password = "12345678"
+password = "password"
 neo4j_driver = GraphDatabase.driver(uri, auth=(username, password))
